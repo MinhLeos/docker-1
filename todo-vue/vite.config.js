0c46b04dca45fb6d61c38a3161ee0a16e56b8cd8
Minh Le <minh.le@mgisolutions.com>
2024-09-26 04:55:30 -0400

    reduce BE docker images size

--- todo-vue/vite.config.js
+++ todo-vue/vite.config.js
@@ -5,9 +5,11 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: '0.0.0.0',
     port: 3000
   },
   preview: {
+    host: '0.0.0.0',
     port: 3000
   }
 })
